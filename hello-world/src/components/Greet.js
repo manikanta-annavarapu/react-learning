3d8d8b02ad9e2f2bfba5d91be1a68129193f1f9c
Manikanta Annavarapu <manikanta.annavarapu95@gmail.com>
2022-12-29 12:56:02 +0530

    function component arrow syntax

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-function Greet(){
-    return <h1>Hello Mani</h1>;
-}
+// function Greet(){
+//     return <h1>Hello Mani</h1>;
+// }
+
+//Arrow function react component
+const Greet = () => <h1>Hello Mani</h1>
 
 export default Greet;
