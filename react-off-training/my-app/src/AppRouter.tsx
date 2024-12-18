2b91d281574d258b06bca2f4947da9bab4d1a2e3
Manikanta Annavarapu <manikanta.annavarapu@intelliflo.com>
2024-12-18 14:40:23 +0530

    implement lazy loading for product list component and add pagination hook documentation

--- react-off-training/my-app/src/AppRouter.tsx
+++ react-off-training/my-app/src/AppRouter.tsx
@@ -5,12 +5,15 @@ import Checkout from "./containers/Checkout";
 import ErrorPage from "./containers/ErrorPage";
 import PrivateRoute from "./components/PrivateRoute";
 import ProductDetail from "./components/ProductDetail";
+import { lazy } from "react";
+
+const LazyProductList = lazy(() => import("./containers/ProductList"));
 
 export default function AppRouter() {
   return (
     <Routes>
       <Route path="/" element={<Demo />} />
-      <Route path="/products" element={<ProductList />} />
+      <Route path="/products" element={<LazyProductList />} />
       <Route path="/detail/:pid" element={<ProductDetail />} />
       <Route
         path="/checkout"
