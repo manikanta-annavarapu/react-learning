411868e010bee38ca1eb949e82b50d6bf3a6eec0
Manikanta Annavarapu <manikanta.annavarapu@intelliflo.com>
2024-12-18 15:26:20 +0530

    Suspense for lazy loading

--- react-off-training/my-app/src/AppRouter.tsx
+++ react-off-training/my-app/src/AppRouter.tsx
@@ -5,25 +5,27 @@ import Checkout from "./containers/Checkout";
 import ErrorPage from "./containers/ErrorPage";
 import PrivateRoute from "./components/PrivateRoute";
 import ProductDetail from "./components/ProductDetail";
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 
 const LazyProductList = lazy(() => import("./containers/ProductList"));
 
 export default function AppRouter() {
   return (
-    <Routes>
-      <Route path="/" element={<Demo />} />
-      <Route path="/products" element={<LazyProductList />} />
-      <Route path="/detail/:pid" element={<ProductDetail />} />
-      <Route
-        path="/checkout"
-        element={
-          <PrivateRoute>
-            <Checkout />
-          </PrivateRoute>
-        }
-      />
-      <Route path="*" element={<ErrorPage />} />
-    </Routes>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Demo />} />
+        <Route path="/products" element={<LazyProductList />} />
+        <Route path="/detail/:pid" element={<ProductDetail />} />
+        <Route
+          path="/checkout"
+          element={
+            <PrivateRoute>
+              <Checkout />
+            </PrivateRoute>
+          }
+        />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Suspense>
   );
 }
