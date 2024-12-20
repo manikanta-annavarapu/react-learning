545d6da742cbc51649d2d69f849d22230715f31b
Manikanta Annavarapu <manikanta.annavarapu@intelliflo.com>
2024-12-20 10:55:28 +0530

    useEventBus for data communication btw mfe

--- react-off-training/shop-app/apps/cart/src/app/app.tsx
+++ react-off-training/shop-app/apps/cart/src/app/app.tsx
@@ -1,11 +1,17 @@
 // Uncomment this line to use CSS modules
 // import styles from './app.module.css';
 import NxWelcome from './nx-welcome';
+import { eventBus, useEventBus } from '@shop-app/event-bus';
 
 export function App() {
+  useEventBus('my-event', (data) => {
+    console.log('Received event in cart mfe', data);
+  });
   return (
     <div>
-      <NxWelcome title="cart" />
+      <button onClick={() => eventBus.emit('my-event', 'Hello from the cart')}>
+        Emit event
+      </button>
     </div>
   );
 }
--- react-off-training/shop-app/apps/shell/src/app/app.tsx
+++ react-off-training/shop-app/apps/shell/src/app/app.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import NxWelcome from './nx-welcome';
 import { Link, Route, Routes } from 'react-router-dom';
+import { useEventBus } from '@shop-app/event-bus';
 
 const Product = React.lazy(() => import('product/Module'));
 const Checkout = React.lazy(() => import('checkout/Module'));
 const Cart = React.lazy(() => import('cart/Module'));
 
 export function App() {
+  useEventBus('my-event', (data) => {
+    console.log('Received event in shell', data);
+  });
+
   window.addEventListener('product_mfe_event', (event) => {
     console.log('Received event in shell from product mfe', event);
   });
